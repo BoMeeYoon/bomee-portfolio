8ae3c0526d5bc041d60b9b685f4bea74adf1158d
bomee <primavera0331@gmail.com>
2020-07-20 02:28:06 +0900

    JAVASCRIPT: Works

--- src/js/portfolioWorksView.js
+++ src/js/portfolioWorksView.js
@@ -1,100 +1,106 @@
-import { $, mount } from "./hooks/elemetsHooks.js";
+import {
+  $,
+  mount,
+  event,
+  removeName,
+  addName,
+  all$,
+} from "./hooks/elemetsHooks.js";
+import { worksData } from "./states/state.js";
 
 const worksViewTemplate = `
   <div class="section__container">
     <h1>My work</h1>
     <h3>Projects</h3>
-    <div class="work__categories">
-      <button class="category__btn selected">
-        All<span class="category__count">3</span>
-      </button>
-      <button class="category__btn">
-        Personal<span class="category__count">1</span>
-      </button>
-      <button class="category__btn">
-        Team<span class="category__count">1</span>
-      </button>
-      <button class="category__btn">
-        etc<span class="category__count">3</span>
-      </button>
-    </div>
-    <div class="work__projects">
-      <div class="work__project">
-        <div class="project__img">
-          <p>😻 주차관리시스템</p>
-          <img src="./images/parkinglot.png" alt="project-photo"/>
-        </div>
-        <div class="project__description">
-          <h3>Parking Lot System</h3>
-          <p>HTML, CSS, Javascript<br/>node.js, express<br/>mySQL<br/>Webpack, Babel</p>
-          <div class="project__description-bts">
-            <a href="https://github.com/BoMeeYoon/vanilla-webpack-parkinglot-mvc" target="blank"><i class="fab fa-github-square"></i>View Details</a>
-          </div>
-        </div>
-      </div>
-      <div class="work__project">
-        <div class="project__img">
-          <p>😻 지출기입장</p>
-          <img src="./images/expenses.png" alt="project-photo"/>
-        </div>
-        <div class="project__description">
-          <h3>Financial Ledger</h3>
-          <p>React.js</p>
-          <div class="project__description-bts">
-            <a href="https://github.com/BoMeeYoon/react-hooks-financial-ledger" target="blank"><i class="fab fa-github-square"></i>View Details</a>
-          </div>
-        </div>
-      </div>
-      <div class="work__project">
-        <div class="project__img">
-          <p>😻 자동검색창</p>
-          <img src="./images/autocomplete.png" alt="project-photo"/>
-        </div>
-        <div class="project__description">
-          <h3>Autocomplete</h3>
-          <p>HTML, CSS, Javascript<br/>Webpack, Babel, Eslint</p>
-          <div class="project__description-bts">
-            <a href="https://github.com/BoMeeYoon/webpack-autocomplete-api-mocker" target="blank"><i class="fab fa-github-square"></i>View Details</a>
-          </div>
-        </div>
-      </div>
-      <div class="work__project">
-        <div class="project__img">
-          <p>😻 온라인교육웹페이지</p>
-          <img src="./images/onlineeducation.png" alt="project-photo"/>
-        </div>
-        <div class="project__description">
-          <h3>Online Coding Education</h3>
-          <p>HTML, CSS, Javascript<br/>
-          Java, Spring, JWT<br/>
-          Python, Django, SSR<br/>
-          MySQL</p>
-          <div class="project__description-bts">
-            <a href="https://github.com/BoMeeYoon/group-online-coding-education/blob/master/%EC%98%A8%EB%9D%BC%EC%9D%B8%EA%B5%90%EC%9C%A1_%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8_%EC%B5%9C%EC%A2%85.pdf" target="blank"><i class="fab fa-github-square"></i>View Details</a>
-          </div>
-        </div>
-      </div>    
-      <div class="work__project">
-        <div class="project__img">
-          <p>😻 주차정산시스템</p>
-          <img src="./images/parkinglotbefore.png" alt="project-photo"/>
-        </div>
-        <div class="project__description">
-          <h3>Autocomplete</h3>
-          <p>HTML, CSS, Javascript, jquery<br/>
-          Node.js, express<br/>
-          MySQL</p>
-          <div class="project__description-bts">
-            <a href="https://github.com/BoMeeYoon/mini-project-in-90factory" target="blank"><i class="fab fa-github-square"></i>View Details</a>
-          </div>
-        </div>
-      </div>    
-    </div>
+    <div class="work__categories"></div>
+    <div class="work__projects"></div>
   </div>
 `;
-
 const mountWorksView = () => mount($("#main__works"), worksViewTemplate);
+
+const categoryHTML = () => {
+  const categoryEl = $(".work__categories");
+  categoryEl.innerHTML = `
+  <button class="category__btn selected" data-filter="*">
+    All<span class="category__count">${worksData.length}</span>
+  </button>
+  <button class="category__btn" data-filter="personal">
+    Personal<span class="category__count">${
+      worksData.filter((data) => data.type === "personal").length
+    }</span>
+  </button>
+  <button class="category__btn" data-filter="team">
+    Team<span class="category__count">${
+      worksData.filter((data) => data.type === "team").length
+    }</span>
+  </button>
+  <button class="category__btn" data-filter="etc">
+    etc<span class="category__count">${
+      worksData.filter((data) => data.type === "etc").length
+    }</span>
+  </button>
+  `;
+};
+
+const getWorksHTML = () => {
+  const projectEl = $(".work__projects");
+  projectEl.innerHTML = worksData.reduce((html, work) => {
+    return (html += getWorkHTML(work));
+  }, "");
+  filterHandler();
+};
+const getWorkHTML = (work) => {
+  const { title, image, description, type } = work;
+  const { engTitle, spec, github } = description;
+
+  return `
+  <div class="work__project" data-type="${type}">
+    <div class="project__img">
+      <p>${title}</p>
+      <img src=${image} alt="project-photo"/>
+    </div>
+    <div class="project__description">
+      <h3>${engTitle}</h3>
+      <p>${spec}</p>
+      <div class="project__description-bts">
+        <a href=${github} target="blank" class="project"><i class="fab fa-github-square"></i>View Details</a>
+      </div>
+    </div>
+  </div>
+  `;
+};
+const filterHandler = () => {
+  const categoryEl = $(".work__categories");
+  const projectContainer = $(".work__projects");
+  const projects = all$(".work__project");
+
+  event(categoryEl, "click", (e) => {
+    const filter =
+      e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (!filter) return;
+    console.log(filter);
+    const active = $(".category__btn.selected");
+    removeName(active, "selected");
+    const target =
+      e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+    addName(target, "selected");
+
+    addName(projectContainer, "anim-out");
+    setTimeout(() => {
+      projects.forEach((project) => {
+        if (filter === "*" || filter === project.dataset.type) {
+          removeName(project, "invisible");
+        } else {
+          addName(project, "invisible");
+        }
+      });
+      removeName(projectContainer, "anim-out");
+    }, 300);
+  });
+};
 export default function init() {
   mountWorksView();
+  categoryHTML();
+  getWorksHTML();
   return this;
 }
--- src/js/states/state.js
+++ src/js/states/state.js
@@ -1,8 +1,8 @@
 import { getElementHeight } from "../hooks/scrollHooks.js";
 import { $ } from "../hooks/elemetsHooks.js";
 export default {
-  headerHeight : getElementHeight($("#header")),
-  headerActive : $(".header__nav-item.active")
+  headerHeight: getElementHeight($("#header")),
+  headerActive: $(".header__nav-item.active"),
 };
 
 export const sceneInfo = [
@@ -54,12 +54,66 @@ export const skillsData = {
       id: 4,
       skill: "REACT",
       proficiency: 50,
-    }
+    },
   ],
-  tools: [
-    "Node.js", "MySQL", "Jquery", "Redux", "Typescript" 
-  ],
-  etc: [
-    "web API", "Git", "Webpack", "Eslint", "Babel"
-  ]
-} 
+  tools: ["Node.js", "MySQL", "Jquery", "Redux", "Typescript"],
+  etc: ["web API", "Git", "Webpack", "Eslint", "Babel"],
+};
+
+export const worksData = [
+  {
+    id: 1,
+    title: `😻 주차관리시스템`,
+    image: `./images/parkinglot.png`,
+    description: {
+      engTitle: `Parking Lot System`,
+      spec: `HTML, CSS, Javascript<br/>node.js, express<br/>mySQL<br/>Webpack, Babel`,
+      github: `https://github.com/BoMeeYoon/vanilla-webpack-parkinglot-mvc`,
+    },
+    type: `personal`,
+  },
+  {
+    id: 2,
+    title: `😻 지출기입장`,
+    image: `./images/expenses.png`,
+    description: {
+      engTitle: `Financial Ledger`,
+      spec: `React.js`,
+      github: `https://github.com/BoMeeYoon/react-hooks-financial-ledger`,
+    },
+    type: `personal`,
+  },
+  {
+    id: 3,
+    title: `😻 자동검색창`,
+    image: `./images/autocomplete.png`,
+    description: {
+      engTitle: `Autocomplete`,
+      spec: `HTML, CSS, Javascript<br/>Webpack, Babel, Eslint`,
+      github: `https://github.com/BoMeeYoon/webpack-autocomplete-api-mocker`,
+    },
+    type: `personal`,
+  },
+  {
+    id: 4,
+    title: `😻 온라인교육웹페이지`,
+    image: `./images/onlineeducation.png`,
+    description: {
+      engTitle: `Online Coding Education`,
+      spec: `HTML, CSS, Javascript<br/>Java, Spring, JWT<br/>Python, Django, SSR<br/>MySQL`,
+      github: `https://github.com/BoMeeYoon/group-online-coding-education/blob/master/%EC%98%A8%EB%9D%BC%EC%9D%B8%EA%B5%90%EC%9C%A1_%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8_%EC%B5%9C%EC%A2%85.pdf`,
+    },
+    type: `team`,
+  },
+  {
+    id: 5,
+    title: `😻 주차정산시스템`,
+    image: `./images/parkinglotbefore.png`,
+    description: {
+      engTitle: `Parking Lot Pay System`,
+      spec: `HTML, CSS, Javascript, jquery<br/>Node.js, express<br/>MySQL`,
+      github: `https://github.com/BoMeeYoon/mini-project-in-90factory`,
+    },
+    type: `personal`,
+  },
+];
